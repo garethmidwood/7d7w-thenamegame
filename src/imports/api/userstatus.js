8ad9c98a21fa134a90156d0de59496d8561f4dc0
Gareth Midwood <gareth.midwood@creode.co.uk>
2020-05-19 13:44:56 +0100

    publish the usernames so others can see who you are

--- src/imports/api/userstatus.js
+++ src/imports/api/userstatus.js
@@ -4,7 +4,7 @@ import { Meteor } from 'meteor/meteor';
 if (Meteor.isServer) {
   console.log('THE SERVER IS PUBLISHING USER STATUS');
   Meteor.publish('userstatus', function userStatusPublication() {
-    return Meteor.users.find({ }, { fields: { "status.online": true }});
+    return Meteor.users.find({ }, { fields: { "status.online": true, "username": true }});
   });
 }
 
