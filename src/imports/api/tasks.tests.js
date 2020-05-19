379894af05f69e9a4aae031d95449911ed808f3c
Gareth Midwood <gareth.midwood@creode.co.uk>
2020-05-19 11:33:36 +0100

    Add more task tests, remove default meteor tests

--- src/imports/api/tasks.tests.js
+++ src/imports/api/tasks.tests.js
@@ -19,10 +19,11 @@ if (Meteor.isServer) {
             createdAt: new Date(),
             owner: userId,
             username: 'tmeasday',
+            private: true,
         });
       });
 
-      it('can delete owned task', () => {
+      it('can delete owned private task', () => {
         // Find the internal implementation of the task method so we can
         // test it in isolation
         const deleteTask = Meteor.server.method_handlers['tasks.remove'];
@@ -35,6 +36,42 @@ if (Meteor.isServer) {
  
         // Verify that the method does what we expected
         assert.equal(Tasks.find().count(), 0);
+      });
+
+      it('can delete owned public task', () => {
+        const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+        const invocation = { userId };
+
+        // make the task public
+        Tasks.update(taskId, { $set: { private: false } });
+ 
+        deleteTask.apply(invocation, [taskId]);
+ 
+        assert.equal(Tasks.find().count(), 0);
+      });
+
+      it('can delete other users public task', () => {
+        const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+        const aDifferentUserId = userId * 2;
+        const invocation = { aDifferentUserId };
+
+        // make the task public
+        Tasks.update(taskId, { $set: { private: false } });
+        
+        deleteTask.apply(invocation, [taskId]);
+ 
+        assert.equal(Tasks.find().count(), 0);
+      });
+
+      it('will throw an error if you try to delete other peoples private tasks', () => {
+        const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+        const aDifferentUserId = userId * 2;
+        const invocation = { aDifferentUserId };
+ 
+        assert.throws(() => {
+          deleteTask.apply(invocation, [taskId]);
+        }, Meteor.Error, /not-authorized/);
+
       });
     });
   });
